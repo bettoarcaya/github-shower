a85d8586f58344ff94ac8a4f908ad505d0c9f84a
Humberto Arcaya <betoarcaya@gmail.com>
2022-12-19 20:47:34 -0400

    feat: installing octokit to consume GH API

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Octokit } from 'octokit';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private octokit: Octokit;
+
+  constructor() {
+    this.octokit = new Octokit({});
+  }
+
+  async getCommitsFromRepo() {
+    const ghResponse = await this.octokit.request(
+      'GET /repos/{owner}/{repo}/commits{?sha,path,author,since,until,per_page,page}',
+      {
+        owner: 'bettoarcaya',
+        repo: 'github-shower',
+      },
+    );
+    return ghResponse;
   }
 }
